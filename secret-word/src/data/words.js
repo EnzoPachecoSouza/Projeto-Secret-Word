82cee393d34308b941f12bf685a55ef463a35d2a
Enzo Pacheco <enzo.pach@gmail.com>
2025-03-27 10:45:31 -0300

    Adicionando uma lista de palavras via JS

---
+++ secret-word/src/data/words.js
@@ -0,0 +1,8 @@
+export const wordsList = {
+carro: ["Motor", "Porta", "Volante", "Pneu", "Freio", "Buzina"],
+fruta: ["Banana", "Maça", "Pera", "Uva", "Laranja", "Manga"],
+corpo: ["Cabeça", "Perna", "Braço", "Ombro", "Pescoço", "Cintura"],
+computador: ["Mouse", "Teclado", "Monitor", "Gabinete", "Fonte", "Placa de Vídeo"],
+programação: ["HTML", "CSS", "JavaScript", "PHP", "Python", "Ruby"],
+alimento: ["Arroz", "Feijão", "Macarrão", "Carne", "Frango", "Peixe"]
+}
