4bdd459536bb2656fa409b3634e317d10fb534cb
Enzo Pacheco <enzo.pach@gmail.com>
2025-03-28 11:37:42 -0300

    Estruturando componente do jogo - game

--- secret-word/src/components/Game/Game.jsx
+++ secret-word/src/components/Game/Game.jsx
@@ -3,9 +3,33 @@ import './Game.css'
 function Game({verifyLetter}) {
     return (
         <div className='game'>
-            <h1>Game</h1>
+            <p className='points'>
+                <span>Pontuação: 000</span>
+            </p>
+
+            <h1>Advinhe a Palavra</h1>
+
+            <h3 className='tip'>
+                Dica sobre a palavra: <span>Dica...</span>
+            </h3>
             
-            <button onClick={verifyLetter}>Fim de jogo</button>
+            <div className='wordContainer'>
+                <span className='letter'>A</span>
+                <span className='blankSquare'></span>
+            </div>
+
+            <div className='letterContainer'>
+                <p>Tente adivinhar uma letra da palavra:</p>
+                <form>
+                    <input type="text" name='letter' maxLength='1' required/>
+                    <button>Jogar</button>
+                </form>
+            </div>
+
+            <div className="wrongLettersContainer">
+                <p>Letras já utilizadas:</p>
+                <span>A,</span>
+            </div>
         </div>
     )
 }
